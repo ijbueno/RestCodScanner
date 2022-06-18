9bf32e3b5517f40ab6d815f6800cb97fbb253d0b
Isaac José Bueno <ijbueno.apks@gmail.com>
2022-06-18 19:15:59 -0300

    Criato Classe para inicializar o serviço do SpringBootApplication

---
+++ src/main/java/br/com/ijbuenoapks/restCodeScanner/RestCodScannerApplication.java
@@ -0,0 +1,13 @@
+package br.com.ijbuenoapks.restCodeScanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestCodScannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestCodScannerApplication.class, args);
+	}
+
+}
